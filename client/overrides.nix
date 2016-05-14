0a38343ae13f57d389f491708c01796ab70e476c
Greg Hale <ImAlsoGreg@gmail.com>
2016-05-14 13:11:17 -0400

    Update overrides.nix

--- client/overrides.nix
+++ client/overrides.nix
@@ -5,5 +5,6 @@ let
 in reflex-platform.ghcjs.override {
   overrides = self: super: {
     reflex-dom-contrib = pkgs.haskell.lib.dontCheck (self.callPackage deps/reflex-dom-contrib {});
+    common = pkgs.haskell.lib.dontCheck (self.callPackage ../common {});
   };
 }
